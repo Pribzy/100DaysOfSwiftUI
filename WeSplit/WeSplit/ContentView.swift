99b70fa9cf72263d3f11ce8925beb285fa235588
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-07 13:52:29 +0200

    Add TextField

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -2,10 +2,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tapCount = 0
+    @State private var name = ""
 
     var body: some View {
-        Button("Tap Count \(tapCount)") {
-            tapCount += 1
+        Form {
+            Button("Tap Count \(tapCount)") {
+                tapCount += 1
+            }
+            TextField("Enter your name", text: $name) // projected value for two-way binding
+            Text("Your name is \(name)")
         }
     }
 }
