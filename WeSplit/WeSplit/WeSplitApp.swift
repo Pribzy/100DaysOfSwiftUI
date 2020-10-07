3a21ee7c2e54893c81e989451631de4ca3ce50ee
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-07 12:54:48 +0200

    Add initial project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pribelszki Levente on 2020. 10. 07..
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
