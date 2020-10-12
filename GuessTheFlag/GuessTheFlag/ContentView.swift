0d2c69b08379f6e97f7da478aaaf50ffdc598a6d
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-12 17:20:34 +0200

    Add alert

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -4,9 +4,14 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.green, .red]), startPoint: .topLeading, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/).edgesIgnoringSafeArea(.all)
-            Text("Your content")
-                .bold()
-                .foregroundColor(.white)
+            Button(action: {
+                print("button tapped")
+            }, label: {
+                HStack(spacing: 10) {
+                    Image(systemName: "pencil")
+                    Text("Tap me")
+                }.foregroundColor(.white)
+            })
         }
     }
 }
