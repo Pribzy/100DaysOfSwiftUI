7d373b4e89a52bcc2d8d726d217cb674c0839aab
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-13 12:04:48 +0200

    Extract view to CapsuleText

--- ViewsAndModifiers/ViewsAndModifiers/ComplexView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ComplexView.swift
@@ -2,12 +2,29 @@ import SwiftUI
 
 struct ComplexView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            CapsuleText(titleText: "First")
+            CapsuleText(titleText: "Second")
+        }
     }
 }
 
 struct ComplexView_Previews: PreviewProvider {
     static var previews: some View {
         ComplexView()
+    }
+}
+
+struct CapsuleText: View {
+
+    var titleText: String
+
+    var body: some View {
+        Text(titleText)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
     }
 }
