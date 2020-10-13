94b455fb6b953af26461644c1fe9624b69a8dc8a
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-13 14:40:35 +0200

    Improve GameMove enum
    Improve with helper methods

--- RockPaperScissors/RockPaperScissors/GameMove.swift
+++ RockPaperScissors/RockPaperScissors/GameMove.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftUI
 
 enum GameMove: CaseIterable {
     case rock
@@ -12,6 +13,30 @@ extension GameMove {
         case .rock: return "Rock"
         case .paper: return "Paper"
         case .scissor: return "Scissor"
+        }
+    }
+
+    var imageName: String {
+        switch self {
+        case .rock: return "book.circle.fill"
+        case .paper: return "doc.fill"
+        case .scissor: return "scissors"
+        }
+    }
+
+    var color: Color {
+        switch self {
+        case .rock: return .red
+        case .paper: return .blue
+        case .scissor: return .green
+        }
+    }
+
+    func correctAnswer(for status: Bool) -> GameMove {
+        switch self {
+        case .rock: return status ? .paper : .scissor
+        case .paper: return status ? .scissor : .rock
+        case .scissor: return status ? .rock: .paper
         }
     }
 }
