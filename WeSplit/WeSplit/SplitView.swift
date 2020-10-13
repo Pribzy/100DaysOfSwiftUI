e6e53fb2205b8a0d0f983595b79fa0a898f2cfa7
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-13 12:44:15 +0200

    Challenge - Add conditional property
    Add conditional property to total amount Text.

--- WeSplit/WeSplit/SplitView.swift
+++ WeSplit/WeSplit/SplitView.swift
@@ -50,6 +50,7 @@ struct SplitView: View {
 
                 Section(header: Text("Total amount")) {
                     Text("$\(totalAmount, specifier: "%.2f")")
+                        .foregroundColor(tipPercentage == 4 ? .red : .black)
                 }
 
             }
