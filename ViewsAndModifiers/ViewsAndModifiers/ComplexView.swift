d189f82a8b9b35ae079488328d1ab60d2f108f78
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-13 11:59:56 +0200

    Create initial ComplexView

---
+++ ViewsAndModifiers/ViewsAndModifiers/ComplexView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ComplexView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComplexView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComplexView()
+    }
+}
