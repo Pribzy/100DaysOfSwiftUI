36147e6e6926a0a284eb62e9e20e228cf618cba5
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-14 09:03:56 +0200

    Create initial project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pribelszki Levente on 2020. 10. 14..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
