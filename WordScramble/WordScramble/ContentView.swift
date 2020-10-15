82a6fe19ab7bb9481e7e7dd5f4f0bdca2bcd0470
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-15 12:07:00 +0200

    Add dynamic and static list

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -1,9 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List {
+                Section(header: Text("Section 1")) {
+                    Text("Static row 1")
+                    Text("Static row 2")
+                }
+
+                Section(header: Text("Section 2")) {
+                    ForEach(0..<5) {
+                        Text("Dynamic row \($0)")
+                    }
+                }
+
+                Section(header: Text("Section 3")) {
+                    Text("Static row 3")
+                    Text("Static row 4")
+                }
+            }.listStyle(GroupedListStyle())
+        }
     }
 }
 
