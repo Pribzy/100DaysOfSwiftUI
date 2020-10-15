a796d84364961abbaae5e607a387fa139c3e4754
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-15 12:09:40 +0200

    Create iteration over array in list

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -5,24 +5,8 @@ struct ContentView: View {
     let people = ["Finn", "Leia", "Luke", "Rey"]
 
     var body: some View {
-        VStack {
-            List {
-                Section(header: Text("Section 1")) {
-                    Text("Static row 1")
-                    Text("Static row 2")
-                }
-
-                Section(header: Text("Section 2")) {
-                    ForEach(0..<5) {
-                        Text("Dynamic row \($0)")
-                    }
-                }
-
-                Section(header: Text("Section 3")) {
-                    Text("Static row 3")
-                    Text("Static row 4")
-                }
-            }.listStyle(GroupedListStyle())
+        List(people, id: \.self) {
+            Text($0)
         }
     }
 }
