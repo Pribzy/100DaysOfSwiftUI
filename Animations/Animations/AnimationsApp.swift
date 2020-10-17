3548118e8d31c6b1eca887af1a860554eec7d766
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-17 19:24:33 +0200

    Add binding animations

--- Animations/Animations/AnimationsApp.swift
+++ Animations/Animations/AnimationsApp.swift
@@ -1,17 +1,10 @@
-//
-//  AnimationsApp.swift
-//  Animations
-//
-//  Created by Pribelszki Levente on 2020. 10. 17..
-//
-
 import SwiftUI
 
 @main
 struct AnimationsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AnimatingBindings()
         }
     }
 }
--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -1,9 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
-
     @State private var animationAmount: CGFloat = 1
-
+    
     var body: some View {
         Button("Tap Me") {
             // self.animationAmount += 1
@@ -26,11 +25,42 @@ struct ContentView: View {
             self.animationAmount = 2
         }
     }
-
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AnimatingBindings: View {
+    @State private var animationAmount: CGFloat = 1
+    
+    var body: some View {
+        print(animationAmount)
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+
+struct AnimatingBindings_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatingBindings()
     }
 }
