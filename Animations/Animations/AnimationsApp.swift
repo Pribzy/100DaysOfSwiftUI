8cfc42d2bc3839ede3034eb4e37a6397feada1e1
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-17 18:52:46 +0200

    Create initial project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Pribelszki Levente on 2020. 10. 17..
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
