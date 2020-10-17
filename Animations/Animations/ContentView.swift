c7a406952c54a07b40b2f3ad81c739dd70b3feb9
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-17 19:52:00 +0200

    Add animation controlling

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -88,3 +88,25 @@ struct ExplicitAnimation_Previews: PreviewProvider {
         ExplicitAnimation()
     }
 }
+
+struct AnimationStack: View {
+    @State private var enabled = false
+
+    var body: some View {
+        Button("Tap Me") {
+            enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(nil)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+    }
+}
+
+struct AnimationStack_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationStack()
+    }
+}
