15e73212f843a43c4daa5241a9c4f155e935d1d2
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-17 20:05:41 +0200

    Add gradient view with dragging animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -110,3 +110,29 @@ struct AnimationStack_Previews: PreviewProvider {
         AnimationStack()
     }
 }
+
+struct GestureAnimations: View {
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+    }
+}
+
+struct GestureAnimations_Previews: PreviewProvider {
+    static var previews: some View {
+        GestureAnimations()
+    }
+}
