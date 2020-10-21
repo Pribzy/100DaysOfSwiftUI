5664620c2bbb948568ab0d54d873b8db940d24da
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-21 14:29:16 +0200

    Create initial project

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Pribelszki Levente on 2020. 10. 21..
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
