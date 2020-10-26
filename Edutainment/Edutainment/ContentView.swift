343590f89519da072dc9f7073adadcb703b4cbf8
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-26 16:33:24 +0100

    Implement challenge

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -1,14 +1,210 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    private var questionCounts: [String] = ["5", "10", "20", "All"]
+
+    @State private var selectedLevel: Int = 0
+    @State private var selectedQuestionCount: Int = 0
+    @State private var isStartGame: Bool = false
+
+    @State private var currentQuestion: Int = 0
+    @State private var correctAnswer: Int = 0
+    @State private var wrongAnswer: Int = 0
+
+    @State private var answer: String = ""
+
+    @State private var showingEndGame: Bool = false
+
+    var questions: [Question] {
+        var allQuestions: [Question] = [Question]()
+        (0..<12).forEach { first in
+            (0..<100).forEach { second in
+                allQuestions.append(Question(first: first, second: second))
+            }
+        }
+        return allQuestions
+    }
+
+    var gameQuestions: [Question] {
+        get {
+            let questionCount = Int(questionCounts[selectedQuestionCount])
+            return Array(questions.choose(questionCount))
+        }
+        set {
+            gameQuestions = newValue
+        }
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if !isStartGame {
+                VStack {
+                    Text("Select level")
+                        .font(.title)
+
+                    LevelView(selectedLevel: $selectedLevel)
+
+                    Text("Question Count")
+                        .font(.title)
+
+                    Picker(selection: $selectedQuestionCount, label: Text("")) {
+                        ForEach(0 ..< questionCounts.count) {
+                            Text(questionCounts[$0])
+                        }
+
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+
+                    Spacer()
+
+                    Button(action: {
+                        withAnimation {
+                            isStartGame.toggle()
+                        }
+                    }, label: {
+                        Text("Start")
+                            .bold()
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                            .padding()
+                            .background(selectedLevel == 0 ? Color.gray.opacity(0.2) : Color.blue)
+                            .animation(.easeInOut)
+
+                    })
+                    .disabled(selectedLevel == 0 ? true : false)
+                    .accentColor(.white)
+                    .cornerRadius(15)
+                }
+                .padding()
+                .transition(.slide)
+            } else {
+                VStack {
+                    Text("Question \(currentQuestion+1)/\(Int(questionCounts[selectedQuestionCount]) ?? 100).")
+                        .font(.largeTitle)
+                    Group {
+                        Text(gameQuestions[currentQuestion].questionString)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                            .background(Color.gray.opacity(0.3))
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                            .font(.title)
+                    }
+                    .padding()
+                    AnswerView(correctAnswer: $correctAnswer, wrongAnswer: $wrongAnswer)
+                    Spacer()
+                    HStack {
+                        TextField("Enter your word", text: $answer)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+                            .autocapitalization(.none)
+                            .keyboardType(.numberPad)
+
+                        Button(action: checkAnswer, label: {
+                            Text("Check")
+                        }).padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
+                    }
+                }
+            }
+        }
+        .alert(isPresented: $showingEndGame) {
+            Alert(title: Text("GAME OVER"),
+                  message: Text("Wanna retry?"),
+                  dismissButton: .destructive(Text("Retry")) {
+                    withAnimation {
+                        isStartGame.toggle()
+                    }
+                  })
+        }
+    }
+
+    private func checkAnswer() {
+        let current = gameQuestions[currentQuestion]
+        if currentQuestion == gameQuestions.count - 1 {
+            showingEndGame = true
+        } else {
+            if (Int(answer) ?? 0) == current.answer {
+                correctAnswer += 1
+                currentQuestion += 1
+            } else {
+                wrongAnswer += 1
+                currentQuestion += 1
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct LevelView: View {
+
+    @Binding var selectedLevel: Int
+
+    var body: some View {
+        VStack {
+            ForEach(1..<4) { row in
+                HStack{
+                    ForEach(1..<5) { column in
+                        let item = column+(row*4)-4
+                        Button(action: {
+                            selectedLevel = item
+                        }, label: {
+                            Text("\(item)")
+                                .bold()
+                                .accentColor(.black)
+                                .padding()
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        })
+                        .background(selectedLevel == item ? Color.yellow.opacity(0.3) : Color.gray.opacity(0.3))
+                        .cornerRadius(10)
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+extension Collection {
+    func choose(_ n: Int?) -> ArraySlice<Element> { shuffled().prefix(n ?? count) }
+}
+
+struct AnswerView: View {
+
+    @Binding var correctAnswer: Int
+    @Binding var wrongAnswer: Int
+
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("\(correctAnswer) correct")
+                .bold()
+                .foregroundColor(.green)
+            Spacer()
+            Text("\(wrongAnswer) wrong")
+                .bold()
+                .foregroundColor(.red)
+            Spacer()
+        }
+    }
+}
+
+struct QuestionView: View {
+    
+    var currentQuestion: String
+
+    var body: some View {
+        Group {
+            Text(currentQuestion)
+                .padding()
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                .background(Color.gray.opacity(0.3))
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .font(.title)
+        }
+        .padding()
     }
 }
