a877dabccad7b8d939d049b63f9b1f336ce58b89
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-27 13:50:31 +0100

    Add example of differences between struct and class

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -1,10 +1,21 @@
 import SwiftUI
 
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State private var user = User()
+
+        var body: some View {
+            VStack {
+                Text("Your name is \(user.firstName) \(user.lastName).")
+
+                TextField("First name", text: $user.firstName)
+                TextField("Last name", text: $user.lastName)
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
