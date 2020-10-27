0e4bed6031f0b4c8a1d4444077c41f21514f87ac
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-27 14:43:36 +0100

    Add UserDefaults

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -91,3 +91,20 @@ struct DeleteView_Previews: PreviewProvider {
         DeleteView()
     }
 }
+
+struct UserDefaultsView: View {
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+
+    var body: some View {
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
+            UserDefaults.standard.set(tapCount, forKey: "Tap")
+        }
+    }
+}
+
+struct UserDefaultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserDefaultsView()
+    }
+}
