1d4d914453805ef14a63d9697ec1392bb325f1dd
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-27 13:56:36 +0100

    Make User Observable

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -1,21 +1,21 @@
 import SwiftUI
 
-struct User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
 }
 
 struct ContentView: View {
-    @State private var user = User()
+    @ObservedObject private var user = User()
 
-        var body: some View {
-            VStack {
-                Text("Your name is \(user.firstName) \(user.lastName).")
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
 
-                TextField("First name", text: $user.firstName)
-                TextField("Last name", text: $user.lastName)
-            }
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
         }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
