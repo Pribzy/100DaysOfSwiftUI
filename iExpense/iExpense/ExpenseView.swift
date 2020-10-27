6f84edcef680a144687f68400463abad781b5ee6
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-27 15:24:44 +0100

    Create base layout for Expenses

---
+++ iExpense/iExpense/ExpenseView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+struct ExpenseView: View {
+
+    @ObservedObject var expenses = Expenses()
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
+    }
+
+    private func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ExpenseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseView()
+    }
+}
