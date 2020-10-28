581314de621b2ff777e538b1ef5f4296f7495830
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-10-28 10:28:03 +0100

    Add ScrollView with Texts

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -13,8 +13,36 @@ struct ImageResizeView: View {
     }
 }
 
+struct ScrollExampleView: View {
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct CustomText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct Previews: PreviewProvider {
     static var previews: some View {
         ImageResizeView()
+        ScrollExampleView()
     }
 }
