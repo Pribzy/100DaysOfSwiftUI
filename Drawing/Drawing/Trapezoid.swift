4cefbf1cac4a4ac16127304aaa8f808f5e1b7ada
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-11-02 10:56:47 +0100

    Create Trapezoid shape

---
+++ Drawing/Drawing/Trapezoid.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
