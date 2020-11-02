37efba6763c9e5c55edcc87e52e135167e411913
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-11-02 11:10:18 +0100

    Create AnimatingView

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -110,6 +110,20 @@ struct GradientCycleView: View {
     }
 }
 
+struct AnimatingView: View {
+    @State private var insetAmount: CGFloat = 50
+
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
+            }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -119,5 +133,6 @@ struct ContentView_Previews: PreviewProvider {
         FlowerView()
         ImagePaintView()
         GradientCycleView()
+        AnimatingView()
     }
 }
