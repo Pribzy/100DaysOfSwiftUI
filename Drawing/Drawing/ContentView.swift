2416ab4f6ea393fb1b9a1cce65c3811427832e9a
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-11-02 10:09:40 +0100

    Change Arc to conform InsettableShape

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -29,10 +29,18 @@ struct ArcView: View {
     }
 }
 
+struct BorderView: View {
+    var body: some View {
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(Color.blue, lineWidth: 40)
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         TriangleView()
         ArcView()
+        BorderView()
     }
 }
