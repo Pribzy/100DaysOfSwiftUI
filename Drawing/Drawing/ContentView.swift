ab50e11df08a95b0f203a0d4be182538ddd89aee
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-11-02 11:15:21 +0100

    Add animatableData to CheckerBoard

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -124,6 +124,21 @@ struct AnimatingView: View {
     }
 }
 
+struct AnimatingPairView: View {
+    @State private var rows = 4
+    @State private var columns = 4
+
+    var body: some View {
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
+                }
+            }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -134,5 +149,6 @@ struct ContentView_Previews: PreviewProvider {
         ImagePaintView()
         GradientCycleView()
         AnimatingView()
+        AnimatingPairView()
     }
 }
