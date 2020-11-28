9fe752f8d5679929bd421150b74bb404a91de0e5
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-11-28 15:57:26 +0100

    Create initial project

---
+++ HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Pribelszki Levente on 2020. 11. 28..
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
