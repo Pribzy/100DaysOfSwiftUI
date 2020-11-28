bef1d94b77b10535f40271cb33151710582af895
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-11-28 16:14:25 +0100

    Add base models

---
+++ HabitTracking/HabitTracking/Models/Activity.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+enum ActivityType: String, Codable {
+    case sport
+    case learning
+    case cook
+}
+
+struct Activity: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let description: String
+    let type: ActivityType
+}
