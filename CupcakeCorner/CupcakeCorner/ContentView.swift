917d844ebcf4c0cb12dcf9df9b426fe8f25e94be
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-12-01 10:36:59 +0100

    Add ValidationView

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -39,8 +39,34 @@ struct ContentView: View {
     }
 }
 
+struct ValidationView: View {
+    @State private var username = ""
+    @State private var email = ""
+
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ValidationView()
     }
 }
--- CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct CupcakeCornerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ValidationView()
         }
     }
 }
