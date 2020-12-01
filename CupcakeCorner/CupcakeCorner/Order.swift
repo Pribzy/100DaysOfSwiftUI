dfde7b90251c1962d0dffd5f734d16f2987e1e40
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-12-01 10:49:49 +0100

    Create Order model

---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    @Published var type = 0
+    @Published var quantity = 3
+
+    @Published var specialRequestEnabled = false
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
