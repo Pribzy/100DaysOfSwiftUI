af12623c59d11d9b48e6fa5c3f96ca500b9bb664
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-12-07 12:26:09 +0100

    Add previews to persistence

--- Bookworm/Bookworm/Persistence.swift
+++ Bookworm/Bookworm/Persistence.swift
@@ -1,10 +1,3 @@
-//
-//  Persistence.swift
-//  Bookworm
-//
-//  Created by Pribelszki Levente on 2020. 12. 07..
-//
-
 import CoreData
 
 struct PersistenceController {
@@ -13,6 +6,15 @@ struct PersistenceController {
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
+        for _ in 0..<10 {
+            let newItem = Book(context: viewContext)
+            newItem.id = UUID()
+            newItem.title = "Test"
+            newItem.genre = "Fantasy"
+            newItem.author = "Me"
+            newItem.review = "Good one!"
+            newItem.rating = 1
+        }
         do {
             try viewContext.save()
         } catch {
@@ -37,13 +39,13 @@ struct PersistenceController {
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 
                 /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
