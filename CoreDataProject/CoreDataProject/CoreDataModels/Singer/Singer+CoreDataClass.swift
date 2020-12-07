6e37592515464e66cd33712d1d43a07bd98e990f
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-12-07 16:47:56 +0100

    Create Singer entity

---
+++ CoreDataProject/CoreDataProject/CoreDataModels/Singer/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Pribelszki Levente on 2020. 12. 07..
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
