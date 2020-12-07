eba93c3684cfcf788ad2def3920d0513fd8ef539
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-12-07 14:57:54 +0100

    Create initial project

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Pribelszki Levente on 2020. 12. 07..
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
