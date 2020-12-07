ebaf74bb9fcfb1b67c83efe8b9fadd0b984fb77e
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2020-12-07 10:14:15 +0100

    Clean project

--- Bookworm/Bookworm/BookwormApp.swift
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -1,20 +1,10 @@
-//
-//  BookwormApp.swift
-//  Bookworm
-//
-//  Created by Pribelszki Levente on 2020. 12. 07..
-//
-
 import SwiftUI
 
 @main
 struct BookwormApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            CoreDataView()
         }
     }
 }
