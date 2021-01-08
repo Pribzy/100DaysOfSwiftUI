f664f26b69ad4d749e2cb4ea16b6f39a69b8cb88
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-01-08 12:26:15 +0100

    Create models
    Create User and Friend model.

---
+++ UsersAndFriends/UsersAndFriends/Models/Friend.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Friend: Codable, Identifiable {
+    var id: String
+    var name: String
+}
---
+++ UsersAndFriends/UsersAndFriends/Models/User.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct User: Identifiable, Codable {
+    var id: String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Friend]
+}
