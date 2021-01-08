354b19bc6f008e635b93202446cd719a0b8fa6f0
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-01-08 13:41:57 +0100

    Implement User list view

--- UsersAndFriends/UsersAndFriends/ContentView.swift
+++ UsersAndFriends/UsersAndFriends/ContentView.swift
@@ -1,9 +1,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(users) { user in
+                VStack(alignment: .leading) {
+                    Text(user.name)
+                        .font(.headline)
+                    Text(user.email)
+                }
+            }
+            .navigationBarTitle("Users")
+            .onAppear(perform: getUsers)
+        }
+    }
+
+    private func getUsers() {
+        URLSession.shared.request(for: .getUsersAndFriends()) { result in
+            switch result {
+            case .success(let users):
+                self.users = users
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
