e5f5a9822392ffd79bb4c3613d020b4d6277706d
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-01-15 22:04:54 +0100

    Create initial project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Pribelszki Levente on 2021. 01. 15..
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
