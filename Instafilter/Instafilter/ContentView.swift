86da73f19889f2424709cdb4673df149008add76
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-01-19 09:50:15 +0100

    Create ActionSheet

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -26,7 +26,7 @@ struct BindingBlurView: View {
             },
             set: {
                 self.blurAmount = $0
-                print("New value is \(self.blurAmount)")
+                print("New value is \(blurAmount)")
             }
         )
 
@@ -39,9 +39,32 @@ struct BindingBlurView: View {
     }
 }
 
+struct ActionSheetView: View {
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
+
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
+            }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
+                    .default(Text("Red")) { backgroundColor = .red },
+                    .default(Text("Green")) { backgroundColor = .green },
+                    .default(Text("Blue")) { backgroundColor = .blue },
+                    .cancel()
+                ])
+            }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         BindingBlurView()
+        ActionSheetView()
     }
 }
