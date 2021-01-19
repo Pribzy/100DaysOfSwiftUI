8e332230e93c42dd07cc36568ed1932a5da07346
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-01-19 09:41:38 +0100

    Create basic blur view

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -1,9 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount: CGFloat = 0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, World!")
+                .blur(radius: blurAmount)
+            Slider(value: $blurAmount, in: 0...20)
+        }
     }
 }
 
