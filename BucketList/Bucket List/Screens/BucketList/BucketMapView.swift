f5026f01302e54be834751b04aab01996185047a
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-01-25 08:58:40 +0100

    Fix files
    Fix file adding.

---
+++ BucketList/Bucket List/Screens/BucketList/BucketMapView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+import MapKit
+
+struct BucketMapView: View {
+
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+
+    var body: some View {
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = centerCoordinate
+                        locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
+    }
+}
+
+struct BucketMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        BucketMapView()
+    }
+}
