c805764d4781ba6254decba95bf200654a3d42a5
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-08-02 14:24:45 +0200

    Add grouping accesibility

--- Accesibility/Shared/ContentView.swift
+++ Accesibility/Shared/ContentView.swift
@@ -30,8 +30,21 @@ struct ContentView: View {
     }
 }
 
+struct GroupingView: View {
+    var body: some View {
+        VStack {
+            Text("Your score is")
+            Text("1000")
+                .font(.title)
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibility(label: Text("Your score is 1000"))
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        GroupingView()
     }
 }
