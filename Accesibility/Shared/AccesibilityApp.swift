5597f900e6e8d605fee47f73a61ee32d14f16d96
Levente Pribelszki <pribelszki.levente@autsoft.hu>
2021-08-02 14:17:11 +0200

    Add image accesibility

---
+++ Accesibility/Shared/AccesibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesibilityApp.swift
+//  Shared
+//
+//  Created by Pribelszki Levente on 2021. 08. 02..
+//
+
+import SwiftUI
+
+@main
+struct AccesibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
